fc842ab7dc06240ff6844ba3c5edab461828182d
Edwin <edwinkaiyin.tam@student.kdg.be>
2018-11-27 18:41:20 +0100

    26/11/2018 13:18

---
+++ src/be/kdg/distanceAPI/DistanceCalculator.java
@@ -0,0 +1,11 @@
+package be.kdg.distanceAPI;
+
+import be.kdg.common.Position;
+
+public class DistanceCalculator {
+    private double distance;
+
+    public double getDistance(Position p1, Position p2){
+        return distance;
+    }
+}
---
+++ src/be/kdg/ui/DeliDishApp.java
@@ -0,0 +1,21 @@
+package be.kdg.ui;
+
+import be.kdg.order.Order;
+import be.kdg.person.Courier;
+
+import java.util.List;
+
+public class DeliDishApp {
+    private String userId;
+
+    private Courier courier;
+
+    private List<Order> beschikbareLeveringen;
+
+    public List<Order> geefBeschikbareLeveringen(){
+
+        //beschikbareLeveringen = getBeschikbareLeveringen();
+
+        return beschikbareLeveringen;
+    }
+}
